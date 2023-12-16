a14aaec00890d6332c85726202788ec31113b53a
Michał Kuciak <michal.kuciak@microsoft.wsei.edu.pl>
2023-12-16 16:27:27 +0100

    Add project files.

---
+++ EncryptionAndDecryption/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EncryptionAndDecryption.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
