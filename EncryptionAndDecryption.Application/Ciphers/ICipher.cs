35b93f6ec857532e9f436c4a488baad661c19529
Michał Kuciak <michal.kuciak@microsoft.wsei.edu.pl>
2023-12-16 17:03:18 +0100

    Added the abstract implementation for any cipher type

---
+++ EncryptionAndDecryption.Application/Ciphers/ICipher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptionAndDecryption.Application.Ciphers
+{
+    public interface ICipher
+    {
+        string EncryptedText(string explicitText);
+        string DecryptedText(string encryptedText);
+    }
+}
---
+++ EncryptionAndDecryption.Application/RemoteControlCiphers.cs
@@ -0,0 +1,18 @@
+﻿using EncryptionAndDecryption.Application.Ciphers;
+
+namespace EncryptionAndDecryption.Application
+{
+    public abstract class RemoteCiphers
+    {
+        protected ICipher _cipher;
+
+        public RemoteCiphers(ICipher cipher)
+        {
+            _cipher = cipher;
+        }
+
+        public abstract void Encrypted();
+        public abstract void Decrypted();
+        //public abstract void Shift();
+    }
+}
