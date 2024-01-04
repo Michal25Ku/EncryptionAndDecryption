7ffab0274e3cccf0f26b99eeaedb0ffb84236eb0
Michał Kuciak <michal.kuciak@microsoft.wsei.edu.pl>
2024-01-04 23:01:04 +0100

    Added PolybiusCipher and HomophonicCipher class, and added RemoteControl class for these objects

---
+++ EncryptionAndDecryption.Application/RemoteControl/CaesarCipherRemoteControl.cs
@@ -0,0 +1,31 @@
+﻿using EncryptionAndDecryption.Application.Ciphers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptionAndDecryption.Application.RemoteControl
+{
+    public class CaesarCipherRemoteControl : RemoteControlCiphers
+    {
+        public CaesarCipherRemoteControl(ICipher cipher) : base(cipher)
+        {
+        }
+
+        public override void ToDecrypt()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ToEncrypt()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ToSetAlphabet()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
