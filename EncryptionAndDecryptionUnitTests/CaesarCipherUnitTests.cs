985a6288101fd76af53c209327fb5145b4b1bba9
Michał Kuciak <michal.kuciak@microsoft.wsei.edu.pl>
2024-01-06 02:22:11 +0100

    Added unit tests for the CaesarCipher class's methods and constructor

---
+++ EncryptionAndDecryptionUnitTests/CaesarCipherUnitTests.cs
@@ -0,0 +1,52 @@
+﻿using EncryptionAndDecryption.Application.Alphabet;
+using EncryptionAndDecryption.Application.Ciphers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptionAndDecryptionUnitTests
+{
+    [TestClass]
+    public class CaesarCipherUnitTests
+    {
+        CaesarCipher _caesarCipher = new CaesarCipher();
+
+        [TestMethod]
+        public void CaesarCipherConstructorTest_ShouldSetShiftTo0_CreateNewInstanceAlphabetsClass_SetDafaultAlphabetToPolish()
+        {
+            CaesarCipher testCaesarCipher = new CaesarCipher();
+
+            Assert.AreEqual(0, testCaesarCipher.Shift);
+            Assert.IsTrue(testCaesarCipher.Alphabets is Alphabets);
+            Assert.IsTrue(testCaesarCipher.CurrentAlphabet.Equals(testCaesarCipher.Alphabets.FoundAlphabet("Pl")));
+        }
+
+        [DataTestMethod]
+        [DataRow(1, "uęyu234")]
+        [DataRow(2, "vfzv345")]
+        [DataRow(3, "wgźw456")]
+        public void DecryptMethodTest_Decrypted_ShouldReturnStringOfDecryptedCryptogram(int shift, string cryptogram)
+        {
+            string result = "text123";
+
+            _caesarCipher.Shift = shift;
+            _caesarCipher.Decrypt(cryptogram);
+
+            Assert.AreEqual(result, _caesarCipher.DecryptedText);
+        }
+
+        [DataTestMethod]
+        [DataRow(1, "text123", "uęyu234")]
+        [DataRow(2, "Test", "vftv")]
+        [DataRow(3, "someThinG", "uqogwklój")]
+        public void EncryptMethodTest_Encrypted_ShouldReturnStringOfEncryptedPlainText(int shift, string cryptogram, string expected)
+        {
+            _caesarCipher.Shift = shift;
+            _caesarCipher.Encrypt(cryptogram);
+
+            Assert.AreEqual(expected, _caesarCipher.EncryptedText);
+        }
+    }
+}
